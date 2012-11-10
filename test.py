ddf9790d0801b99e4f2d457617e7141e3b94b935
Pierre-Henri Schoffit <ph.schoffit@insa-lyon.fr>
2012-11-10 19:42:02 +0100

    Some wonderful stuff

--- test.py
+++ test.py
@@ -1,22 +1,43 @@
 from kivy.app import App
 from kivy.core.image import Image
+from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.clock import Clock
 
 class MyGrid(Widget):
-    def __init__(self):
-        super(MyGrid,self).__init__()
-        texture = Image('art/ground.png').texture
-        texture.wrap = 'repeat'
-        texture.uvsize = (32, 32)
-        with self.canvas:
-            Color(1, 1, 1)
-            Rectangle(pos=(0, 0), size=(2000, 2000), texture=texture)
+    x1 = NumericProperty(150)
+    y1 = NumericProperty(100)
+    x2 = NumericProperty(100)
+    y2 = NumericProperty(200)
+    x3 = NumericProperty(200)
+    y3 = NumericProperty(200)
+    points = ReferenceListProperty(x1, y1, x2, y2, x3, y3)
+
+    def update(self, dt):
+        self.x1, self.y1 = Window.mouse_pos
+        self.x2, self.y2 = self.x1 - 50, self.y1 + 100
+        self.x3, self.y3 = self.x1 + 50, self.y1 + 100
+        print self.points
+
+#   def __init__(self):
+#      super(MyGrid,self).__init__()
+#       texture = Image('art/wall2.png').texture
+#       texture.wrap = 'repeat'
+#       texture.uvsize = (32, 32)
+#       with self.canvas:
+#           Color(1, 1, 1)
+#           Rectangle(pos=(0, 0), size=(2000, 2000), texture=texture)
+
+#   def addTorchStencil(x, y):
+#       pass
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        mg = MyGrid()
+        Clock.schedule_interval(mg.update, 0.5)
+        return mg
 
 if __name__ == '__main__':
     MyApp().run()
