bcfd09a675289da103e2601358b044c3187849d4
Nicolas Dossou-Gbété <ndossougbe@mail>
2012-11-10 22:27:00 +0100

    collision done

--- main.py
+++ main.py
@@ -4,26 +4,31 @@
 from os.path import join
 from kivy.app import App
 from kivy.core.image import Image
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, StencilPush, StencilUse, StencilPop, StencilUnUse, Triangle
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.vector import Vector
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
 from kivy.clock import Clock
-
+from kivy.factory import Factory
 
 import logging
 
 from slmap import SLMap
 
+map = None
+
 class SpylightGame(Widget):
 
     def __init__(self, **kwargs):
         super(SpylightGame, self).__init__(**kwargs)
-        # self.add_widget(MapView(self.size))
 
 class MapView(Widget):
+    width = NumericProperty(0)
+    height = NumericProperty(0)
+    groundTexture = ObjectProperty(None)
+
     def getTexture(self,name, size):
         filename = join('art', name+'.png')
         texture = Image(filename).texture
@@ -32,65 +37,54 @@ class MapView(Widget):
         self.logger.info(filename)
         return texture
 
-    def __init__(self, map):
+    def __init__(self, map, spy):
         super(MapView, self).__init__()
-        self.logger = logging.getLogger("SpylightApp")
         tileSize = 32
-        ground = self.getTexture(name='ground', size=(32,32))
-        wall = self.getTexture(name='wall', size=(32,32))
+        self.logger = logging.getLogger("SpylightApp")
+        self.width = map.width*tileSize
+        self.height = map.height*tileSize
+        self.groundTexture = self.getTexture(name='ground', size=(32,32))
+        # ground = self.getTexture(name='ground', size=(32,32))
+        # wall = self.getTexture(name='wall', size=(32,32))
+        
+        print spy.points
 
-        with self.canvas:
-            Color(1, 1, 1)
-            Rectangle(pos=(0,0), size=(map.width*tileSize, map.height*tileSize), texture=ground)
-
+        with self.canvas:    
+            StencilPush()
+            Triangle(points=spy.points)
+            StencilUse()
+            Rectangle(pos=(0,0), size=(self.width, self.height), texture=self.groundTexture)
+            StencilUnUse()
+            Triangle(points=spy.points)
+            StencilPop()
+            
+        # with self.canvas:
         for x in xrange(map.width):
             for y in xrange(map.height):
                 if map.getWallType(x, y) != -1:
                     wall = Wall()
                     wall.pos = (x*tileSize, y*tileSize)
-                    self.add_widget(wall)
-                    
+                    self.add_widget(wall)            
 
 class Wall(Widget):
     pass
 
 
 
-class SpylightApp(App):
-    def initLogger(self):
-        self.logger = logging.getLogger("SpylightApp")
-        self.logger.addHandler(logging.FileHandler("spylight.log"))
-        self.logger.setLevel(logging.INFO)
-
-    def build(self):
-        self.initLogger()
-
-        map = SLMap("test.map")
-        self.logger.info("Map loaded: " + map.title)
-        self.logger.info("Map size: (" + str(map.width) + ", " + str(map.height) + ")")
-
-        self.logger.info("What in (1, 1): " + str(map.getWallType(1, 1)))
-        spy = Spy()
-        game = SpylightGame()
-        game.add_widget(MapView(map=map))
-        game.add_widget(spy)
-
-        Clock.schedule_interval(spy.update, 1.0/60.0)
-        return game
 
 class Spy(Widget):
-    # velocity_x = NumericProperty(0)
-    # velocity_y = NumericProperty(0)
-    # velocity = ReferenceListProperty(velocity_x, velocity_y)
+    x1 = NumericProperty(0)
+    y1 = NumericProperty(0)
+    x2 = NumericProperty(0)
+    y2 = NumericProperty(100)
+    x3 = NumericProperty(100)
+    y3 = NumericProperty(100)
+    points = ReferenceListProperty(x1, y1, x2, y2, x3, y3)
 
     def __init__(self, **kwargs):
         super(Spy, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-
-        # with self.canvas:
-        #     Color(0.5,0.5,0.5)
-        #     Rectangle(size=(10,10), pos=self.pos)
 
     def _keyboard_closed(self):
         print 'My keyboard have been closed!'
@@ -102,46 +96,71 @@ class Spy(Widget):
         print ' - text is %r' % text
         print ' - modifiers are %r' % modifiers
 
+        pos2 = self.pos
+
         # Keycode is composed of an integer + a string
         # If we hit escape, release the keyboard
         if keycode[1] == 'escape':
             keyboard.release()
         if keycode[1] == 'z':
             # self.velocity[1] = 1;
-            self.pos = Vector(0,2) + self.pos
+            pos2 = Vector(0,2) + self.pos
         if keycode[1] == 'q':
             # self.velocity[0] = -1;
-            self.pos = Vector(-2,0) + self.pos
+            pos2 = Vector(-2,0) + self.pos
         if keycode[1] == 's':
             # self.velocity[1] = -1;
-            self.pos = Vector(0,-2) + self.pos
+            pos2 = Vector(0,-2) + self.pos
         if keycode[1] == 'd':
             # self.velocity[0] = 1;
-            self.pos = Vector(2,0) + self.pos
+            pos2 = Vector(2,0) + self.pos
 
-
-        # print 'bleh', self.velocity[0]
-        # print '_on_keyboard_down',self.velocity
-        # Return True to accept the key. Otherwise, it will be used by
-        # the system.
+        if(self.canGo(pos2)):
+            self.pos = pos2
         return True
 
     def update(self, truc, **kwargs):
+        self.x1, self.y1 = self.pos
+        self.x2, self.y2 = self.x1 - 50, self.y1 + 100
+        self.x3, self.y3 = self.x1 + 50, self.y1 + 100
+        # print self.pos, self.points
         pass
-        # self.deceleration = 0.5
-        # self.pos = Vector(*self.velocity) + self.pos
-        # print 'update',self.velocity
 
-        # if self.velocity_x < 0:
-        #     self.velocity_x = self.velocity_x + self.deceleration
-        # elif self.velocity_x > 0:
-        #     self.velocity_x = self.velocity_x - self.deceleration
-        # if self.velocity_y < 0:
-        #     self.velocity_y = self.velocity_y + self.deceleration
-        # elif self.velocity_y > 0:
-        #     self.velocity_y = self.velocity_y - self.deceleration
+    def canGo(self,pos2):
+        ret = map.getWallType((pos2[0]+5)/32, (pos2[1]+5)/32) == -1
+        ret = ret and map.getWallType((pos2[0]+27)/32, (pos2[1]+27)/32) == -1
+        ret = ret and map.getWallType((pos2[0]+5)/32, (pos2[1]+27)/32) == -1
+        ret = ret and map.getWallType((pos2[0]+27)/32, (pos2[1]+5)/32) == -1
+        print pos2, pos2[0]/32,pos2[1]/32
+
+        return ret
 
 
+Factory.register("MapView", MapView)
+
+class SpylightApp(App):
+    def initLogger(self):
+        self.logger = logging.getLogger("SpylightApp")
+        self.logger.addHandler(logging.FileHandler("spylight.log"))
+        self.logger.setLevel(logging.INFO)
+
+    def build(self):
+        global map
+        self.initLogger()
+
+        map = SLMap("test.map")
+        self.logger.info("Map loaded: " + map.title)
+        self.logger.info("Map size: (" + str(map.width) + ", " + str(map.height) + ")")
+
+        self.logger.info("What in (1, 1): " + str(map.getWallType(1, 1)))
+        spy = Spy()
+
+        game = SpylightGame()
+        game.add_widget(MapView(map=map, spy=spy))
+        game.add_widget(spy)
+
+        Clock.schedule_interval(spy.update, 1.0 / 60.0)
+        return game
 
 if __name__ == '__main__':
     SpylightApp().run()
