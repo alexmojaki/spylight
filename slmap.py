13242fbc5ae2bedc33b2219a88cc7a4d93d4302f
Pierre-Henri Schoffit <ph.schoffit@insa-lyon.fr>
2012-11-10 14:27:13 +0100

    Basic parsing (header reading) done for map

--- slmap.py
+++ slmap.py
@@ -6,4 +6,19 @@ class SLMap:
         self.load(filename)
 
     def load(self, filename):
-        pass
+        mapfile = open(filename, "r")
+        if mapfile.readline() != "SLMap 1\n":
+            return False
+        headerStage = 0 # No header line parsed yet.
+        for line in mapfile:
+            if line[0] == "#":
+                continue
+            elif headerStage == 0:
+                self.title = line.strip()
+                headerStage += 1 # Now, we know the title of the map
+            elif headerStage == 1:
+                self.x, self.y = [int(_) for _ in line.strip().split(" ")]
+                headerStage = -1 # Now, we know the size of the map
+                                 # Header reading done
+            else:
+                pass
