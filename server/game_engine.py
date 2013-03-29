e78c6992f254c14b7235685451c848815c16e710
tdubourg <plop@plop.fake>
2013-03-29 11:59:33 +0100

    WIP on issue #4 . Not working at all right now

--- server/game_engine.py
+++ server/game_engine.py
@@ -7,9 +7,19 @@ from config_handler import ConfigHandler
 
 class Player(object):
     """Player class, mainly POCO object"""
+    PLAYER_RADIUS = 5
     def __init__(self, player_id):
         super(Player, self).__init__()
         self.player_id = player_id
+        self.posx = 0 
+        self.posy = 0 
+
+class Weapon(object):
+    """Weapong class, mainly a POCO object"""
+    def __init__(self, _range, angle_error):
+        super(Weapon, self).__init__()
+        self.range = _range
+        self.angle_error = angle_error
         
 
 class GameEngine(object):
@@ -37,8 +47,10 @@ class GameEngine(object):
                                  #       read from the map file.
         # Loading players
         self._players = [Player(i) for i in xrange(0, self._player_number)]
+        # Do some things like settings the weapon for each player...
 
     def load_map(self, map_file):
+        pass
 
     def get_nb_players(self):
         return self._player_number
@@ -48,6 +60,23 @@ class GameEngine(object):
 
     def updateMovementDir(self, pid, angle):
         self._players[pid].movAngle = angle
+
+    def shoot(self, pid, angle):
+        # This vector/line represents the trajectory of the bullet
+        vector = LineString(((self._players[pid].posx, self._players[pid].posy), (self._players[pid].posx + self._players[pid].weapon.range, self._players[pid].weapon.range)))
+        # First, check if we could even potentially shoot any player
+        victims = []
+        for p in self._players:
+            # Yes, we do compute the player's hitbox on shoot. It is in fact lighter that storing it in the player, because storing it in the player's object would mean
+            # updating it on every player's move. Here we do computation only on shoots, we are going to be many times less frequent that movements!
+            hitbox = Point(p.posx,p.posy).buffer(Player.PLAYER_RADIUS)
+            if vector.intersects(hitbox): # hit!
+                victims.append(p)
+
+        # Then, if yes, check that there is not any obstacle to that shoot
+        # Only check on obstacles that are close to that shoot's trajectory (that is to say, not < (x,y) (depending on the angle, could be not > (x,y) or event more complex cases, but that's the idea)))
+        if 0 != len(victims):
+                    
 
     def shutdown(self, force=False):
         self._loop.set()
