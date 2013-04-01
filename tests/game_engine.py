9e32d27756a378f3e00845a5e6c8f50bef1f793f
tdubourg <plop@plop.fake>
2013-04-02 01:14:33 +0200

    WIP issue #12

--- tests/game_engine.py
+++ tests/game_engine.py
@@ -3,6 +3,7 @@
 import sys
 import unittest
 sys.path.append("../")
+from server.game_engine import Player
 
 from server.game_engine import GameEngine
 
@@ -14,6 +15,18 @@ class GameEngineTest(unittest.TestCase):
 
 	def getGE(self):
 		return GameEngine().init(self.conf_file, self.map_file)
+	
+	# This is dangerous, do not attempt at home
+	# This is used to access private stuff easily
+	# testing purposes only
+	def __get_private(self, obj, name):
+		return getattr(obj, "_" + obj.__class__.__name__ + name)
+
+	# Just an alias for __get_private, because the original name is too damn long
+	# but this stuff being what it is, I don't want it to have another
+	# name that would not document it
+	def __gp(self, o, n):
+		return self.__get_private(o, n)
 		
 	def test_instanciate(self):
 		self.getGE()
@@ -25,6 +38,24 @@ class GameEngineTest(unittest.TestCase):
 	def test_load_config(self):
 		ge = self.getGE()
 		self.assertTrue(ge.config.send_state_interval == 0.02)
+
+	def test___actionable_item_key_from_row_col(self):
+		ge = self.getGE()
+		result = self.__gp(ge, "__actionable_item_key_from_row_col")(1, 2)
+		expected = "1,2"
+		self.assertTrue(result == expected)
+	
+	def test_shoot(self):
+		ge = self.getGE()
+		players = self.__gp(ge, "__players")
+		p1, p2 = Player(0, 0), Player(1, 1)
+		p1.posx = 0
+		p1.posy = 0
+		p2.posx = 10
+		p2.posy = 10
+		players[0] = p1
+		players[1] = p2
+
 
 if __name__ == '__main__':
 	unittest.main()
