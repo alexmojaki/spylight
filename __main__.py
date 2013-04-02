41900de373221855ebbf22992a24d30eab816a5b
Pierre-Henri Schoffit <ph@pHbang.(none)>
2013-04-02 17:09:34 +0200

    Fix a bug with `__main__.py' argument handling.

--- __main__.py
+++ __main__.py
@@ -5,8 +5,8 @@ from server.spylight_server import SpylightServer
 
 
 if __name__ == '__main__':
-    if not len(sys.argv) == 3:
-        print 'usage: {} host port'.format(sys.argv[0])
+    if not len(sys.argv) == 4 or sys.argv[1] != '-s':
+        print 'usage: {} -s host port'.format(sys.argv[0])
         sys.exit(1)
 
-    SpylightServer(sys.argv[1], int(sys.argv[2]), '').start()
+    SpylightServer(sys.argv[2], int(sys.argv[3]), '').start()
