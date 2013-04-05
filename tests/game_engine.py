25ef1e3cfcfcfd0aa2707b8f997cc0ae356bcadc
tdubourg <plop@plop.fake>
2013-04-05 13:55:18 +0200

    Added a new map and a new test. Beginning to test action() method. issues #12 and #5

--- tests/game_engine.py
+++ tests/game_engine.py
@@ -57,7 +57,7 @@ class GameEngineTest(unittest.TestCase):
     def __check_is_not_harmed(self, player, original_health):
         self.assertTrue(player.hp == original_health, "The player should NOOOOOT have been shot (hp, orig_hp)=" + str((player.hp, original_health)) + ". The shot should have been stopped by an obstacle.")
 
-    def test_easy_straight_horizontal_line_shoot(self):
+    def test_easy_straight_horizontal_line_shoot(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -74,7 +74,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_harmed(p2, original_health)
 
-    def test_obstructed_straight_horizontal_line_shoot(self):
+    def test_obstructed_straight_horizontal_line_shoot(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -94,7 +94,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_not_harmed(p2, original_health)
 
-    def test_obstructed_straight_diagonal_line_shoot(self):
+    def test_obstructed_straight_diagonal_line_shoot(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -114,7 +114,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_not_harmed(p2, original_health)
 
-    def test_shot_in_hole_in_the_wall(self):
+    def test_shot_in_hole_in_the_wall(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -134,7 +134,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_harmed(p2, original_health)
 
-    def test_shot_in_hole_in_the_wall_2(self):
+    def test_shot_in_hole_in_the_wall_2(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -154,7 +154,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_harmed(p2, original_health)
 
-    def test_shot_in_hole_in_the_wall_3(self):
+    def test_shot_in_hole_in_the_wall_3(self):# TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -174,7 +174,7 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_harmed(p2, original_health)
 
-    def test_shot_not_aiming(self):
+    def test_shot_not_aiming(self): # TODO: Refactor using __setup_two_players new method
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         players = self.__gp(ge, "__players")
@@ -194,6 +194,27 @@ class GameEngineTest(unittest.TestCase):
         ge.shoot(id_p1, shoot_angle)
         self.__check_is_not_harmed(p2, original_health)
 
+    def __setup_two_players(self, ge):
+        players = self.__gp(ge, "__players")
+        id_p1, id_p2 = 0, 1
+        p1, p2 = Player(id_p1, 0), Player(id_p2, 1)
+        _range, angle_error, dps = 10000, 0.0, 10
+        p1.weapon, p2.weapon = GunWeapon(_range, angle_error, dps), GunWeapon(_range, angle_error, dps)
+        original_health = p2.hp
+        (p1.posx, p1.posy) = mt((2, 6), const.CELL_SIZE)
+        (p2.posx, p2.posy) = mt((7, 6), const.CELL_SIZE)
+        if DBG:
+            print "Player1 pos=", (p1.posx, p1.posy)
+            print "Player2 pos=", (p2.posx, p2.posy)
+        players[id_p1] = p1
+        players[id_p2] = p2
+        return (id_p1, p1), (id_p2, p2)
+
+    def test_simplistic_action_does_not_crash(self):
+        self.map_file = "map_test_action.hfm"
+        ge = self.getGE()
+        setup = self.__setup_two_players(ge)
+        ge.action(setup[0][0])
 
 if __name__ == '__main__':
     unittest.main()
