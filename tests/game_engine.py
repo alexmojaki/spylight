ca02008e311f84f4d9613f91d7a0d4a9b15c7011
tdubourg <plop@plop.fake>
2013-04-07 00:46:47 +0200

    Fixed the angles in the movement tests to respect the kivy convention + added new tests. All tests passed. Issues #12 and #6

--- tests/game_engine.py
+++ tests/game_engine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from math import radians, cos
 
 import sys
 import unittest
@@ -228,7 +229,7 @@ class GameEngineTest(unittest.TestCase):
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         row, col = 3, 3
-        posx, posy, speedx, speedy, move_angle = row*const.CELL_SIZE, col*const.CELL_SIZE, 1.0, 1.0, -45
+        posx, posy, speedx, speedy, move_angle = row*const.CELL_SIZE, col*const.CELL_SIZE, 1.0, 1.0, 315
         players = self.__setup_players(ge, [(col, row)])
         pid = players[0].player_id
         ge.set_movement_angle(pid, move_angle)
@@ -241,13 +242,31 @@ class GameEngineTest(unittest.TestCase):
         self.map_file = "map_test_scinded.hfm"
         ge = self.getGE()
         row, col = 0, 0
-        posx, posy, speedx, speedy, move_angle = row*const.CELL_SIZE, col*const.CELL_SIZE, 1.0, 1.0, -45
+        posx, posy, speedx, speedy, move_angle = row*const.CELL_SIZE, col*const.CELL_SIZE, 1.0, 1.0, 315
         players = self.__setup_players(ge, [(col, row)])
         p = players[0]
         pid = players[0].player_id
         p.max_speedx = 2*const.CELL_SIZE
         p.max_speedy = 2*const.CELL_SIZE
-        expected_posx, expected_posy = posx + speedx * p.max_speedx, posy + speedy * p.max_speedy # easy calculation as -45 degrees = (1, 1) direction vector 
+        diagonal_move_reduc_coeff = cos(radians(45))
+        expected_posx, expected_posy = posx + speedx * diagonal_move_reduc_coeff * p.max_speedx, posy + speedy * diagonal_move_reduc_coeff * p.max_speedy
+        ge.set_movement_angle(pid, move_angle)
+        ge.set_movement_speedx(pid, speedx)
+        ge.set_movement_speedy(pid, speedy)
+        ge.step()
+        self.assertTrue(abs(p.posx-expected_posx) <= 0.01 and abs(p.posy-expected_posy) <= 0.01, "The player coordinates should have been closer to expected ones. \nExpected: " + str((expected_posx, expected_posy)) + "\nResult: " + str((p.posx, p.posy)))
+
+    def test_simplistic_move_with_speed_check2(self):
+        self.map_file = "map_test_scinded.hfm"
+        ge = self.getGE()
+        row, col = 0, 0
+        posx, posy, speedx, speedy, move_angle = row*const.CELL_SIZE, col*const.CELL_SIZE, 0.0, 1.0, 0
+        players = self.__setup_players(ge, [(col, row)])
+        p = players[0]
+        pid = players[0].player_id
+        p.max_speedx = 2*const.CELL_SIZE
+        p.max_speedy = 4*const.CELL_SIZE
+        expected_posx, expected_posy = posx, posy + speedy * p.max_speedy
         ge.set_movement_angle(pid, move_angle)
         ge.set_movement_speedx(pid, speedx)
         ge.set_movement_speedy(pid, speedy)
