ecd319507a337116391628bdd4bc9e2e89326d03
Pierre-Henri Schoffit <ph.schoffit@free.fr>
2013-04-29 21:30:03 +0200

    Fix server shutdown process.

--- server/threading_tcp_server.py
+++ server/threading_tcp_server.py
@@ -3,6 +3,7 @@
 
 from game_engine import GameEngine
 from SocketServer import ThreadingMixIn, TCPServer
+from threading import Thread, Event
 
 from stoppable_thread import StoppableThread
 
@@ -11,15 +12,49 @@ class ThreadingTCPServer(ThreadingMixIn, TCPServer, object):
     def __init__(self, address, request_handler, reuse_address=False):
         self.allow_reuse_address = reuse_address
         super(ThreadingTCPServer, self).__init__(address, request_handler)
-        self.server_thread = None
+        self._stopped = Event()
+        self._server_thread = None
+        self._handler_threads = {}
+        print 'TCP server waiting for connections on {}, port {}.'.format(
+            *address)
 
     def get_request(self):
         sock, addr = super(ThreadingTCPServer, self).get_request()
         sock.settimeout(GameEngine().config.handle_timeout)
         return sock, addr
 
-    def serve_forever(self, start=True):
-        self.handler_thread = StoppableThread(target=super(ThreadingTCPServer,
-                                                           self).serve_forever)
-        if start:
-            self.handler_thread.start()
+    def process_request(self, request, client_address):
+        try:
+            self._handler_threads[client_address]
+        except KeyError:
+            pass
+        else:
+            print 'Connection error: a connection from', client_address[0]
+            print '                  with source port', client_address[1],
+            'is already open.'
+            return
+
+        t = StoppableThread(target = self.process_request_thread, args = (
+            request, client_address))
+        self._handler_threads[client_address] = t
+        t.daemon = self.daemon_threads
+        t.start()
+
+    def serve_forever(self):
+        self._server_thread = Thread(target=super(ThreadingTCPServer, self).
+                                     serve_forever)
+        self._stopped.clear()
+        self._server_thread.start()
+
+    def shutdown(self, force=False):
+        self._stopped.set()
+        super(ThreadingTCPServer, self).shutdown()
+
+        for t in self._handler_threads.values():
+            t.stop(force)
+        for t in self._handler_threads.values():
+            t.join()
+        self._server_thread.join()
+
+    def is_stopped(self):
+        return self._stopped.is_set()
