alexmojaki/spylight

commit ea08e27522080e49d93bb45b84be4b3b00f74fc2

fixed the vision bug. Verified only with the spy

client/character.py
@@ -50,27 +50,26 @@ class PlayerVision(KVStringAble):
 '''
 
     def get_v(self):
-        print 'get_v'
         return self._v
 
     def set_v(self, val):
-        print 'set_v'
         self._v = val
 
     def get_i(self):
-        print 'get_i'
         return self._i
 
     def set_i(self, val):
-        print 'set_i'
         self._i = val
 
     vertices = AliasProperty(get_v, set_v, bind=('dummy_toggle',))
     indices = AliasProperty(get_i, set_i, bind=('dummy_toggle',))
 
     def update(self, vision_data):
-        self._i = range(0, len(vision_data)/4)
         self._v = vision_data
+        for i in range(0, len(vision_data), 4):
+            self._v[i] += self.char.offsetx
+            self._v[i+1] += self.char.offsety
+        self._i = range(0, len(vision_data)/4)
         # Tell kivy to update the mesh
         self.dummy_toggle = not self.dummy_toggle
 

client/app.py
@@ -7,7 +7,7 @@ from client.utils import spritePath
 # Changing kivy specific settings: must be done before loading other stuff.
 from kivy.config import Config  # http://kivy.org/docs/api-kivy.config.html
 Config.set('kivy', 'window_icon', spritePath.format('mercenary'))
-Config.set('kivy', 'log_level', 'debug')  # Change here to see debug messages
+Config.set('kivy', 'log_level', 'info')  # Change here to see debug messages
 # Config.set('kivy', 'log_enable', '0')
 # Config.set('graphics', 'width', '400')
 # Config.set('graphics', 'height', '400')

client/map_view.py
@@ -91,7 +91,8 @@ class MapView(RelativeWidget):
         la = []
         for l_area in self.lightened_areas:
             la.append(l_area.kv_string('self.' + l_area.kvname))
-        la.append(self.char.get_vision().kv_string('self.char.get_vision()'))
+        self.char_vision = self.char.get_vision()
+        la.append(self.char.get_vision().kv_string('self.char_vision'))
 
         h = []
         for hidden_obj in self.hidden:

test2.hfm
@@ -9,5 +9,5 @@ size: 9 9
 |       |
 +-----+ |
 |     | |
-|     | |
+|       |
 +-----+-+

commit cebf3cb78589d01b5222ed36a5bd85051970858a

Fixed a bug introduced in ea08e27522080e49d93bb45b84be4b3b00f74fc2 where an error would cause the character not to turn/move anymore

client/character.py
@@ -65,10 +65,12 @@ class PlayerVision(KVStringAble):
     indices = AliasProperty(get_i, set_i, bind=('dummy_toggle',))
 
     def update(self, vision_data):
-        self._v = vision_data
+        self._v = []
         for i in range(0, len(vision_data), 4):
-            self._v[i] += self.char.offsetx
-            self._v[i+1] += self.char.offsety
+            self._v.append(vision_data[i] + self.char.offsetx)
+            self._v.append(vision_data[i+1] + self.char.offsety)
+            self._v.append(0)
+            self._v.append(0)
         self._i = range(0, len(vision_data)/4)
         # Tell kivy to update the mesh
         self.dummy_toggle = not self.dummy_toggle